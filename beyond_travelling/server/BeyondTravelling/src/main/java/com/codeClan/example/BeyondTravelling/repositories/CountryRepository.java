ba0950f73e4d05372974856c98d2b7b805743ae4
belus90 <belus90@gmail.com>
2022-05-28 19:25:56 +0100

    set up repositories

---
+++ beyond_travelling/server/BeyondTravelling/src/main/java/com/codeClan/example/BeyondTravelling/repositories/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.codeClan.example.BeyondTravelling.repositories;
+
+import com.codeClan.example.BeyondTravelling.models.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
