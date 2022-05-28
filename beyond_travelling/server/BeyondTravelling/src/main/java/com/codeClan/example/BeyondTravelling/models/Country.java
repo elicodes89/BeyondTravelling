0e5adeab37e2fa72abf5bd82ce02e459ab61c101
belus90 <belus90@gmail.com>
2022-05-28 19:02:46 +0100

    country table working

---
+++ beyond_travelling/server/BeyondTravelling/src/main/java/com/codeClan/example/BeyondTravelling/models/Country.java
@@ -0,0 +1,42 @@
+package com.codeClan.example.BeyondTravelling.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+//    private List<City> cityList;
+
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public Country() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
