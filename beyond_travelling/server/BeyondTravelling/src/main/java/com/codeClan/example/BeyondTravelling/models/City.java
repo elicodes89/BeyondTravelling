bdb7a5ca4e8ea97f030912330a6275a484888418
belus90 <belus90@gmail.com>
2022-05-28 19:07:03 +0100

    city table working

--- beyond_travelling/server/BeyondTravelling/src/main/java/com/codeClan/example/BeyondTravelling/models/City.java
+++ beyond_travelling/server/BeyondTravelling/src/main/java/com/codeClan/example/BeyondTravelling/models/City.java
@@ -1,4 +1,41 @@
 package com.codeClan.example.BeyondTravelling.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "cities")
 public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+//    private List<Hotel> hotelList;
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public City() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
