1a2fc28f4abcac55a23da3e9119bb58eb174d688
belus90 <belus90@gmail.com>
2022-05-28 19:16:48 +0100

    hotel table working

--- beyond_travelling/server/BeyondTravelling/src/main/java/com/codeClan/example/BeyondTravelling/models/Hotel.java
+++ beyond_travelling/server/BeyondTravelling/src/main/java/com/codeClan/example/BeyondTravelling/models/Hotel.java
@@ -1,4 +1,87 @@
 package com.codeClan.example.BeyondTravelling.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "hotels")
 public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hotel_name")
+    private String hotel_name;
+
+    @Column(name = "contact_number")
+    private String contact_number;
+
+    @Column(name = "website")
+    private String website;
+
+    @Column(name = "longitude")
+    private String longitude;
+
+    @Column(name = "latitude")
+    private String latitude;
+
+    public Hotel(String hotel_name, String contact_number, String website, String longitude, String latitude) {
+        this.hotel_name = hotel_name;
+        this.contact_number = contact_number;
+        this.website = website;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Hotel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHotel_name() {
+        return hotel_name;
+    }
+
+    public void setHotel_name(String hotel_name) {
+        this.hotel_name = hotel_name;
+    }
+
+    public String getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
 }
+
