d56e147f85785524bc313554c160626ee0fc60e3
belus90 <belus90@gmail.com>
2022-05-28 18:53:10 +0100

    commit

---
+++ beyond_travelling/server/BeyondTravelling/src/main/java/com/codeClan/example/BeyondTravelling/BeyondTravellingApplication.java
@@ -0,0 +1,13 @@
+package com.codeClan.example.BeyondTravelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondTravellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondTravellingApplication.class, args);
+	}
+
+}
---
+++ beyond_travelling/server/BeyondTravelling/src/test/java/com/codeClan/example/BeyondTravelling/BeyondTravellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeClan.example.BeyondTravelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondTravellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
