5618fc0b696eabef76b9a2e381ce4f3475cebd68
elicodes89 <elicodes89@gmail.com>
2022-05-30 16:08:51 +0100

    able to see the regions on the frontend

--- beyond_travelling/client/new_app/src/App.js
+++ beyond_travelling/client/new_app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import MainPage from './containers/MainPage';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
     <img className="bsl_sign" src={require("./access_icons/signing1.jpeg")} alt='Signing'/>
     <img className="wheelchair" src={require("./access_icons/wheelchair.jpeg")} alt='Wheelchair'/>
     </div>
+    <MainPage />
+    
     <footer>2022 - Created by Annika, Eli and Mate - </footer>
     </>
   );
---
+++ beyond_travelling/client/new_app/src/components/wheelchair/HotelDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HotelDetail = ({region, onRegionClick}) => {
+
+  const handleClick = function(){
+    onRegionClick(region);
+  }
+
+  return <li onClick={handleClick}>{region.name}</li>
+}
+
+export default HotelDetail;
