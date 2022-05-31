a0ed3771195f1d68ee798976d732e7445c5cc094
Annika Neumann <annika1983neumann@gmail.com>
2022-05-31 19:46:36 +0100

    solved bootstrap issues

--- beyond_travelling/client/new_app/src/App.js
+++ beyond_travelling/client/new_app/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
-import MainPage from './containers/MainPage';
+// import MainPage from './containers/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card'
+import Home from './containers/Home';
+// import Card from 'react-bootstrap/Card'
 
 
 
 function App() {
+
+    return (
+        <>
+        <h1>Hello</h1>
+        <Home />
+        </>
+    )
 }
+
 
 export default App;
