2e6395d360ec46f0cf780416faf16cee244278ed
elicodes89 <elicodes89@gmail.com>
2022-06-03 14:13:20 +0100

    css touches on home page

--- beyond_travelling/client/new_app/src/containers/Home.js
+++ beyond_travelling/client/new_app/src/containers/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-
-
+import './Home.css';
 
 function Home () {
 
@@ -9,14 +8,14 @@ return (
   <>
   <Header />
     <div className='title'>
-    <h2 className>Your travel app for accessible holidays</h2>
+    <h2>Your travel app for accessible holidays</h2>
     </div>
     
     <div className='flex-container'>
 
 {/* have clickable images as hyperlink */}
-    <a href='src/components/bsl/BSL.js'><img className="bsl_sign" src={require("../access_icons/signing1.jpeg")} alt='Signing'/></a>
-    <a href='src/components/wheelchair/AccessibleHotels.js'><img className="wheelchair" src={require("../access_icons/wheelchair.jpeg")} alt='Wheelchair'/></a>
+    <a href='src/components/bsl/BSL.js'> <img className="bsl_sign" src={require("../access_icons/wheelchair.png")} alt='wheelchairimg' width="500" height="500"/> </a>
+    <a href='src/components/wheelchair/AccessibleHotels.js'> <img className="wheelchair" src={require("../access_icons/sign.png")} alt='signimg' width="500" height="500"/> </a>
     </div>
   </>
 
