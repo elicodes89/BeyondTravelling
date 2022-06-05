c24e2086470ba7297d3465bd4affca75f15ce16a
belus90 <belus90@gmail.com>
2022-06-05 13:43:17 +0100

    Descriptions almost finished

--- beyond_travelling/server/BeyondTravelling/src/main/java/com/codeClan/example/BeyondTravelling/components/DataLoader.java
+++ beyond_travelling/server/BeyondTravelling/src/main/java/com/codeClan/example/BeyondTravelling/components/DataLoader.java
@@ -70,7 +70,7 @@ public class DataLoader implements ApplicationRunner {
         hotelRepository.save(hotel10);
 
         //This is the hotel in Dundee
-        Hotel hotel11 = new Hotel("Staybridge Suites","020 3966 8654","https://www.guestreservations.com","-2.9624006001947434","56.465844944513066","Lower Dens Mill, Dundee, DD4 6AD, United Kingdom",city3,"https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Scotland/Dundee/StaybridgeCollage.jpg","https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Scotland/Dundee/Staybridge1.jpeg");
+        Hotel hotel11 = new Hotel("Staybridge Suites","020 3966 8654","https://www.guestreservations.com","-2.9624006001947434","56.465844944513066","Lower Dens Mill, Dundee, DD4 6AD, United Kingdom",city4,"https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Scotland/Dundee/StaybridgeCollage.jpg","https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Scotland/Dundee/Staybridge1.jpeg");
         hotelRepository.save(hotel11);
 
         //This is the hotel in Skye
@@ -112,15 +112,15 @@ public class DataLoader implements ApplicationRunner {
         cityRepository.save(city16);
 
         //These are the hotels in Cardiff
-        Hotel hotel20 = new Hotel("Cardiff1","020 3966 8654","google.com","-2.9624006001947434","56.465844944513066","Cardiff city",city14,"https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Cardiff/HomefieldCollage.jpg","");
+        Hotel hotel20 = new Hotel("Homefield Holidays","(+44) 01495 764212","https://homefield.wales/"," -2.866228647627431","51.9165695231888","Grosmont, Abergavenny NP7 8EP",city14,"https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Cardiff/HomefieldCollage.jpg","");
         hotelRepository.save(hotel20);
 
         //These are the hotels in Swansea
-        Hotel hotel22 = new Hotel("Swansea","020 3966 8654","google.swansee.com","-2.9624006001947434","56.465844944513066","swansee eh45 kb",city15,"https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Swansea/StableCollage.jpeg","https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Swansea/Stable2.jpeg");
+        Hotel hotel22 = new Hotel("The Stable","07368577232","https://www.walesgowercottages.co.uk/the-stable","-4.0944747","51.637405439890365","Blaen Cedi Farm SA4 4LX",city15,"https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Swansea/StableCollage.jpeg","https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Swansea/Stable2.jpeg");
         hotelRepository.save(hotel22);
 
         //These are the hotels in Newport
-        Hotel hotel24 = new Hotel("Newport","020 3966 8654","google.newport","-2.9624006001947434","56.465844944513066","Lower Dens Mill, Dundee, DD4 6AD, United Kingdom",city16,"https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Newport/CuffernManorCollage.jpg","https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Newport/Cuffernmanor4.jpeg");
+        Hotel hotel24 = new Hotel("Cuffernmanor","01437 710071","https://www.cuffernmanor.co.uk/","-5.0567761362527","51.8514641148183","Pembrokeshire SA62 6HB",city16,"https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Newport/CuffernManorCollage.jpg","https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Wales/Newport/Cuffernmanor4.jpeg");
         hotelRepository.save(hotel24);
 
         //This is the cities in Ireland
@@ -143,15 +143,15 @@ public class DataLoader implements ApplicationRunner {
         Hotel hotel4 = new Hotel("Donegal Hotel","+353749541112","https://www.woodhillhouse.com/","-8.403013666358087","54.75839283544279","Wood Road Ardara Donegal F94 E102 Ireland",city8,"https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Ireland/Donegal/WoodCollage.jpg","https://raw.githubusercontent.com/elicodes89/BeyondTravelling/develop/beyond_travelling/client/new_app/src/regions/Ireland/Donegal/woodhillhouse3.jpeg");
         hotelRepository.save(hotel4);
 
-//       //This is the comment for Inverness
-//        Description description1 = new Description("Delicious meals from early breakfast till dinner",hotel10);
-//        descriptionRepository.save(description1);
-//        Description description2 = new Description("Our guest rooms have black-out curtains, great for good sleep.",hotel10);
-//        descriptionRepository.save(description2);
-//        Description description3 = new Description("Only 3 minute walk from Inverness Airport, easily accessible transport to Inverness city centre.",hotel10);
-//        descriptionRepository.save(description3);
-//        Description description4 = new Description("Business or leisure, your stay at hotel will be very comfortable",hotel10);
-//        descriptionRepository.save(description4);
+        //This is the comment for Inverness
+        Comment description1 = new Comment("Delicious meals from early breakfast till dinner",hotel10);
+        commentRepository.save(description1);
+        Comment description2 = new Comment("Our guest rooms have black-out curtains, great for good sleep.",hotel10);
+        commentRepository.save(description2);
+        Comment description3 = new Comment("Only 3 minute walk from Inverness Airport, easily accessible transport to Inverness city centre.",hotel10);
+        commentRepository.save(description3);
+        Comment description4 = new Comment("Business or leisure, your stay at hotel will be very comfortable",hotel10);
+        commentRepository.save(description4);
 
         //This is the section for videos
         Video vide1 = new Video("https://www.youtube.com/embed/s0NFHo2RyMg", hotel5);
@@ -163,7 +163,7 @@ public class DataLoader implements ApplicationRunner {
 //        Video vide4 = new Video("https://www.youtube.com/embed/s0NFHo2RyMg", hotel5);
 //        videoRepository.save(vide4);
 //
-//        //This is the comments for Dundee
+         //This is the comments for Dundee
         Comment description5 = new Comment("Hotel with gym, shared lounge and 24-hour front desk.",hotel11);
         commentRepository.save(description5);
         Comment description6 = new Comment("Continental and buffet breakfast options are available each morning",hotel11);
@@ -188,9 +188,8 @@ public class DataLoader implements ApplicationRunner {
         commentRepository.save(description14);
         Comment description15 = new Comment("Edinburgh Castle Tour in Bsl available",hotel5);
         commentRepository.save(description15);
-//
-//
-//        //Edinburgh The Bonham
+
+        //Edinburgh The Bonham
         Comment description16 = new Comment("49 individually designed bedrooms.",hotel6);
         commentRepository.save(description16);
         Comment description17 = new Comment("10 minute walk to Princes Street/Gardens, great views of Edinburgh Castle.",hotel6);
@@ -201,8 +200,8 @@ public class DataLoader implements ApplicationRunner {
         commentRepository.save(description19);
         Comment description20 = new Comment("Lift access to every floor, guide dog friendly hotel.",hotel6);
         commentRepository.save(description20);
-//
-//        //St Andrews LODGE 6 AT ELDERBURN LUXURY LODGES
+
+        //St Andrews LODGE 6 AT ELDERBURN LUXURY LODGES
         Comment description21 = new Comment("5 self-catering lodges.",hotel7);
         commentRepository.save(description21);
         Comment description22 = new Comment("Designed and built to suit people who may have additional access requirements.",hotel7);
@@ -212,7 +211,7 @@ public class DataLoader implements ApplicationRunner {
         Comment description24 = new Comment("Additional grab rails and wetroom with shower.",hotel7);
         commentRepository.save(description24);
 
-//        //St Andrews CULMORE BRIDGE COTTAGES
+        //St Andrews CULMORE BRIDGE COTTAGES
         Comment description25 = new Comment("John and Jacqui welcome all guests at Culmore Bridge Cottages.",hotel8);
         commentRepository.save(description25);
         Comment description26 = new Comment("3 spacious, comfortable bungalows set in peaceful woodland area.",hotel8);
@@ -223,8 +222,8 @@ public class DataLoader implements ApplicationRunner {
         commentRepository.save(description28);
         Comment description29 = new Comment("Wheelchair friendly furniture and carpets. Kitchens have some low-level worktops, access under hob and sink.",hotel8);
         commentRepository.save(description29);
-//
-//        //Ireland Dublin
+
+        //Ireland Dublin
         Comment description30 = new Comment("Overlooking a leafy park, Hotel only 5 minutes by shuttle bus from Dublin Airport.",hotel1);
         commentRepository.save(description30);
         Comment description31 = new Comment("15 minutes by car from Dublin City Centre.",hotel1);
@@ -241,8 +240,8 @@ public class DataLoader implements ApplicationRunner {
         commentRepository.save(description36);
         Comment description37 = new Comment("Exciting events in Dublin available : for example at the Little Museum, an Irish Sign Language Tour – click here https://www.littlemuseum.ie/irish-sign-language-tour\n",hotel1);
         commentRepository.save(description37);
-//
-//        //Maldron Belfast
+
+       //Maldron Belfast
         Comment description38 = new Comment("Maldron Hotel is located right in the heart of Belfast city.",hotel2);
         commentRepository.save(description38);
         Comment description39 = new Comment("Walking distance to restaurants and historic landmarks.",hotel2);
@@ -252,7 +251,7 @@ public class DataLoader implements ApplicationRunner {
         Comment description41 = new Comment("Very wheelchair accessible, visual and vibration alarm, Braille in elevator, Loop System at reception. ",hotel2);
         commentRepository.save(description41);
 
-//        //Galway Belfast
+        //Galway Belfast
         Comment description42 = new Comment("The Dean Galway is your home, right on doorstep of Wild Atlantic Way!",hotel3);
         commentRepository.save(description42);
         Comment description43 = new Comment("Hotel offers 100 super comfortable rooms.",hotel3);
@@ -262,7 +261,7 @@ public class DataLoader implements ApplicationRunner {
         Comment description45 = new Comment("Hotel has 4 ground floor rooms, one with full wheelchair access & a wet room",hotel3);
         commentRepository.save(description45);
 
-//        //Donegal
+        //Donegal
         Comment description46 = new Comment("Historic coastal house built 17th century. House overlooks beautiful Donegal Highlands.",hotel4);
         commentRepository.save(description46);
         Comment description47 = new Comment("Half mile from sea and one quarter from coastal town Ardara on the ‘Wild Atlantic Way’",hotel4);
@@ -274,6 +273,95 @@ public class DataLoader implements ApplicationRunner {
         Comment description50 = new Comment("Emergency cord in bathroom, lower bathroom sink, higher level toilet, toilet with grab railsr",hotel4);
         commentRepository.save(description50);
 
+        //Skye
+        Comment description51 = new Comment("",hotel9);
+        commentRepository.save(description51);
+        Comment description52 = new Comment("",hotel9);
+        commentRepository.save(description52);
+        Comment description53 = new Comment("",hotel9);
+        commentRepository.save(description53);
+        Comment description54 = new Comment("",hotel9);
+        commentRepository.save(description54);
+        Comment description55 = new Comment("",hotel9);
+        commentRepository.save(description55);
+
+        //Newport Wales
+        Comment description56 = new Comment("",hotel24);
+        commentRepository.save(description56);
+        Comment description57 = new Comment("",hotel24);
+        commentRepository.save(description57);
+        Comment description58 = new Comment("",hotel24);
+        commentRepository.save(description58);
+        Comment description59 = new Comment("",hotel24);
+        commentRepository.save(description59);
+        Comment description60 = new Comment("",hotel24);
+        commentRepository.save(description60);
+
+        //Swansee Wales
+        Comment description61 = new Comment("",hotel22);
+        commentRepository.save(description61);
+        Comment description62 = new Comment("",hotel22);
+        commentRepository.save(description62);
+        Comment description63 = new Comment("",hotel22);
+        commentRepository.save(description63);
+        Comment description64 = new Comment("",hotel22);
+        commentRepository.save(description64);
+        Comment description65 = new Comment("",hotel22);
+        commentRepository.save(description65);
+
+        //Cardiff
+        Comment description66 = new Comment("",hotel20);
+        commentRepository.save(description66);
+        Comment description67 = new Comment("",hotel20);
+        commentRepository.save(description67);
+        Comment description68 = new Comment("",hotel20);
+        commentRepository.save(description68);
+        Comment description69 = new Comment("",hotel20);
+        commentRepository.save(description69);
+        Comment description70 = new Comment("",hotel20);
+        commentRepository.save(description70);
+
+        //Liverpool
+        Comment description71 = new Comment("",hotel18);
+        commentRepository.save(description71);
+        Comment description72 = new Comment("",hotel18);
+        commentRepository.save(description72);
+        Comment description73 = new Comment("",hotel18);
+        commentRepository.save(description73);
+
+        //London
+        Comment description74 = new Comment("",hotel12);
+        commentRepository.save(description74);
+        Comment description75 = new Comment("",hotel12);
+        commentRepository.save(description75);
+        Comment description76 = new Comment("",hotel12);
+        commentRepository.save(description76);
+        Comment description77 = new Comment("",hotel12);
+        commentRepository.save(description77);
+        Comment description78 = new Comment("",hotel12);
+        commentRepository.save(description78);
+
+        //Manchaster
+        Comment description79 = new Comment("",hotel14);
+        commentRepository.save(description79);
+        Comment description80 = new Comment("",hotel14);
+        commentRepository.save(description80);
+        Comment description81 = new Comment("",hotel14);
+        commentRepository.save(description81);
+        Comment description82 = new Comment("",hotel14);
+        commentRepository.save(description82);
+        Comment description83 = new Comment("",hotel14);
+        commentRepository.save(description83);
+
+        //Birmigham
+        Comment description84 = new Comment("",hotel16);
+        commentRepository.save(description84);
+        Comment description85 = new Comment("",hotel16);
+        commentRepository.save(description85);
+        Comment description86 = new Comment("",hotel16);
+        commentRepository.save(description86);
+        Comment description87 = new Comment("",hotel16);
+        commentRepository.save(description87);
 
     }
 
