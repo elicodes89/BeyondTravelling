62682e19af07b3e72fdf1b41952830b9f22c95ea
Annika83 <annika1983neumann@gmail.com>
2022-06-08 15:28:33 +0100

    playing around with sign image and disclaimer

--- beyond_travelling/client/new_app/src/containers/Home.js
+++ beyond_travelling/client/new_app/src/containers/Home.js
@@ -2,7 +2,9 @@ import React , { useState } from 'react';
 // import Header from '../components/Header';
 import './Home.css';
 import Wheelchair from '../access_icons/wheelchair.png';
-import Sign from '../access_icons/sign.png';
+// import Sign from '../access_icons/sign.png';
+import Signinghello from '../access_icons/signinghello.gif';
+
 import BSL from '../components/bsl/BSL';
 import Button from 'react-bootstrap/Button';
 import AccessibleHotels from '../components/wheelchair/AccessibleHotels';
@@ -21,8 +23,8 @@ return (
     <h2>YOUR TRAVEL APP FOR ACCESSIBLE HOLIDAYS</h2>
     </div>
     <div className='homeButtons'>
-    <img className = "homeImage" onClick = { () => {setWheelchair(true)} } src={Wheelchair} alt='wheelchairimg' width="500" height="500"/>
-    <img className = "homeImage" onClick = { () => {setBsl(true)} } src={Sign} alt='signimg' width="500" height="500"/>
+    <img className = "homeImage" onClick = { () => {setWheelchair(true)} } src={Wheelchair} alt='wheelchairimg' width="500" height="450"/>
+    <img className = "homeImage" onClick = { () => {setBsl(true)} } src={Signinghello} alt='signimg' width="500" height="450" margin-bottom='0%'/>
     </div>
   </>
 
